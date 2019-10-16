bbcaf0041d87cf7e0aa04c476dd87eaab0119886
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-10-16 18:40:00 +0300

    Scripted auto-commit on change (2019-10-16 18:40:00) by gitwatch.sh

--- src/main/java/mapreduce/apache/ApacheReduce.java
+++ src/main/java/mapreduce/apache/ApacheReduce.java
@@ -12,6 +12,7 @@ public class ApacheReduce {
         JavaSparkContext sc = new JavaSparkContext(conf);
         JavaRDD<String> flights = sc.textFile("664600583_T_ONTIME_sample.csv");
         JavaRDD<String> airports = sc.textFile("L_AIRPORT_ID.csv");
+        
 
     }
 }
