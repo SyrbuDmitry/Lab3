37221f90377b6e53d77a9b57fa43c922fdde4292
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-10-16 18:27:35 +0300

    Scripted auto-commit on change (2019-10-16 18:27:35) by gitwatch.sh

--- src/main/java/mapreduce/apache/ApacheReduce.java
+++ src/main/java/mapreduce/apache/ApacheReduce.java
@@ -8,8 +8,8 @@ public class ApacheReduce {
     public static void main(String[] args){
         SparkConf conf = new SparkConf().setAppName("lab5");
         JavaSparkContext sc = new JavaSparkContext(conf);
-        JavaRDD<String> flights = sc.textFile("/Users/dmitrijsyrbu/Lab3/664600583_T_ONTIME_sample.csv");
-        JavaRDD<String> airports = sc.textFile("/Users/dmitrijsyrbu/Lab3/L_AIRPORT_ID.csv");
+        JavaRDD<String> flights = sc.textFile("664600583_T_ONTIME_sample.csv");
+        JavaRDD<String> airports = sc.textFile("L_AIRPORT_ID.csv");
         JavaRDD<String> splitedFlights = flights.flatMap()
     }
 }
