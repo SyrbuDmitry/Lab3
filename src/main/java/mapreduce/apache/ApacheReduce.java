2079bba93590576490c0c3d5e63be3203af5fcd2
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-10-16 18:18:16 +0300

    Scripted auto-commit on change (2019-10-16 18:18:16) by gitwatch.sh

--- src/main/java/mapreduce/apache/ApacheReduce.java
+++ src/main/java/mapreduce/apache/ApacheReduce.java
@@ -1,12 +1,13 @@
 package mapreduce.apache;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
 public class ApacheReduce {
     public static void main(String[] args){
         SparkConf conf = new SparkConf().setAppName("lab5");
-        JavaSparkContext sc =new JavaSparkContext(conf);
-        
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        JavaRDD<String> flights = 
     }
 }
