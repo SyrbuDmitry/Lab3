0b0ab607077cd5d102de37b39571ff56b8078807
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-10-16 19:07:32 +0300

    Scripted auto-commit on change (2019-10-16 19:07:32) by gitwatch.sh

--- src/main/java/mapreduce/apache/ApacheReduce.java
+++ src/main/java/mapreduce/apache/ApacheReduce.java
@@ -14,7 +14,7 @@ public class ApacheReduce {
         JavaSparkContext sc = new JavaSparkContext(conf);
         JavaRDD<String> flights = sc.textFile("664600583_T_ONTIME_sample.csv");
         JavaRDD<String> airports = sc.textFile("L_AIRPORT_ID.csv");
-        JavaRDD<String> flightsSplited = flights.flatMap(s->Arrays.stream(s.split(" ")).iterator()
+        JavaRDD<String[]> flightsSplited = flights.map(s->Arrays.stream(s.split(" "))
         );
         JavaPairRDD<Tuple2<String,String>,FlightLine> = flights.mapToPair(
                 s->Arrays.stream(s.split(" ")))
