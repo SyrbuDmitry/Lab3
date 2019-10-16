fab4853a207656fe77c6eeffb5f84b5443973279
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-10-16 18:53:04 +0300

    Scripted auto-commit on change (2019-10-16 18:53:04) by gitwatch.sh

--- src/main/java/mapreduce/apache/FlightLine.java
+++ src/main/java/mapreduce/apache/FlightLine.java
@@ -1,6 +1,11 @@
 package mapreduce.apache;
 
+import scala.Tuple2;
+
 import java.io.Serializable;
 
 public class FlightLine implements Serializable {
+    public Tuple2<String,String> flightKey;
+    public String delay;
+    
 }
