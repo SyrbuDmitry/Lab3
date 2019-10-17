d6c8959670030de955235805d7b134771421e0b9
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-10-17 15:06:38 +0300

    Scripted auto-commit on change (2019-10-17 15:06:38) by gitwatch.sh

--- src/main/java/mapreduce/apache/ApacheReduce.java
+++ src/main/java/mapreduce/apache/ApacheReduce.java
@@ -32,6 +32,6 @@ public class ApacheReduce {
 //            }
 //        });
 
-        flightsSplited.saveAsTextFile("/user/dmitrijsyrbu/sparkoutput");
+        f.saveAsTextFile("/user/dmitrijsyrbu/sparkoutput");
     }
 }
