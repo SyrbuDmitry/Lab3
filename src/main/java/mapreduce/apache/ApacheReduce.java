62709d4b68d9df93de698c0b1b7b91b55a27607e
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-10-17 13:34:30 +0300

    Scripted auto-commit on change (2019-10-17 13:34:30) by gitwatch.sh

--- src/main/java/mapreduce/apache/ApacheReduce.java
+++ src/main/java/mapreduce/apache/ApacheReduce.java
@@ -4,6 +4,7 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function2;
 import scala.Tuple2;
 
 import java.util.Arrays;
@@ -19,8 +20,11 @@ public class ApacheReduce {
         );
         JavaPairRDD<Tuple2<String,String>,FlightLine> f = flightsSplited.mapToPair(
                 s->new Tuple2<>(new Tuple2<>(s[11],s[14]),new FlightLine(s[18],s[19])));
-        JavaPairRDD<Tuple2<String,String>,FlightLine> res = f.reduceByKey(Function2<FlightLine,FlightLine,FlightLine>(){
-            
-        })
+        JavaPairRDD<Tuple2<String,String>,FlightLine> res = f.reduceByKey(new Function2<FlightLine,FlightLine,FlightLine>(){
+            @Override
+            public FlightLine call(FlightLine a,FlightLine b){
+
+            }
+        });
     }
 }
