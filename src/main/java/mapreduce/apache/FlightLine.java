83b0a08f7ba0e6fc000f87681593a8219fd900c2
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-10-17 15:13:32 +0300

    Scripted auto-commit on change (2019-10-17 15:13:32) by gitwatch.sh

--- src/main/java/mapreduce/apache/FlightLine.java
+++ src/main/java/mapreduce/apache/FlightLine.java
@@ -7,14 +7,13 @@ import java.io.Serializable;
 public class FlightLine implements Serializable {
     public double delay;
     public boolean isCanceled;
-    public FlightLine(String delay, String canceled){
-
+    public FlightLine(String d, String canceled){
         if(canceled.equals("1.00")) {
             this.delay = -1;
             isCanceled = true;
         } else {
             isCanceled = false;
-            this.delay = Double.parseDouble(delay);
+            this.delay = Double.parseDouble(d);
         }
     }
 }
