a40bc74c9c79bb2272b57e22d9481e3a8848dc01
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-10-17 13:29:56 +0300

    Scripted auto-commit on change (2019-10-17 13:29:56) by gitwatch.sh

--- src/main/java/mapreduce/apache/ApacheReduce.java
+++ src/main/java/mapreduce/apache/ApacheReduce.java
@@ -19,6 +19,6 @@ public class ApacheReduce {
         );
         JavaPairRDD<Tuple2<String,String>,FlightLine> f = flightsSplited.mapToPair(
                 s->new Tuple2<>(new Tuple2<>(s[11],s[14]),new FlightLine(s[18],s[19])));
-        
+
     }
 }
